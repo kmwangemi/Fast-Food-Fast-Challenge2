f6e9ab4964168b036b28bf062d8325f73ad06728
kmwangemi <mwangemik@gmail.com>
2019-12-29 21:09:37 +0300

    adding travis file and integration

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![Build Status](https://travis-ci.org/kmwangemi/Fast-Food-Fast-Challenge2.svg?branch=develop)](https://travis-ci.org/kmwangemi/Fast-Food-Fast-Challenge2)
+
+
 # Fast-Food-Fast-Challenge2
 Fast-Food- Fast is a food delivery service app for a restaurant
--- tests/v1/test_order.py
+++ tests/v1/test_order.py
@@ -6,13 +6,13 @@ from app import create_app
 from app.api.v1.views.order_view import Order
 
 
-class OrderTestCase(TestCase):
+class TestOrderCase(TestCase):
     """The base class for order tests and tearing down"""
 
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app(config_name="testing")
-        self.client = self.app.test_client
+        self.client = self.app.test_client()
         self.headers = {"content-type": "application/json"}
         self.no_json_headers = {}
         self.order = {
@@ -28,13 +28,13 @@ class OrderTestCase(TestCase):
 
     def test_order_created_successfully(self):
         """Test API can create an order successfully (POST request)"""
-        res = self.client().post('/api/v1/orders', data=json.dumps(self.order), headers = self.headers)
+        res = self.client.post('/api/v1/orders', data=json.dumps(self.order), headers = self.headers)
         self.assertEqual(res.status_code, 201)
         self.assertIn('Order made successfully', str(res.data))
     
     def test_order_cannot_create_with_invalid_details(self):
         """Tests that an order cannot be created with empty fields"""
-        res = self.client().post('/api/v1/orders', data=json.dumps(self.empty_order), headers = self.headers)
+        res = self.client.post('/api/v1/orders', data=json.dumps(self.empty_order), headers = self.headers)
         self.assertEqual(res.status_code, 201)
 
 
